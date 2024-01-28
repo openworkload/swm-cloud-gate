3803fc7499de068b022b172c2000a9ba027c03f9
Taras Shapovalov <taras@iclouds.net>
2024-01-28 23:36:23 +0400

    Format

--- app/routers/azure/connector.py
+++ app/routers/azure/connector.py
@@ -1,11 +1,9 @@
-import http
+import json
 import logging
 import typing
 
 import jinja2
 import yaml
-import json
-
 from azure.core.exceptions import HttpResponseError
 from azure.identity import CertificateCredential
 from azure.mgmt.resource import ResourceManagementClient
@@ -59,10 +57,23 @@ class AzureConnector(BaseConnector):
         else:
             LOG.error("Not enough parameters provided to initialize Azure connection")
 
-    def _get_deployment_properties(self, job_id: str, flavor_name: str, os_version: str, username: str, user_pub_cert_path: str,) -> dict[str, dict[str, typing.Any]]:
+    def _get_deployment_properties(
+        self,
+        job_id: str,
+        flavor_name: str,
+        os_version: str,
+        username: str,
+        user_pub_cert_path: str,
+    ) -> dict[str, dict[str, typing.Any]]:
         with open(TEMLPATE_FILE) as template_file:
             template = json.load(template_file)
-        template_parameters = self._get_template_parameters(job_id, flavor_name, os_version, username, user_pub_cert_path,)
+        template_parameters = self._get_template_parameters(
+            job_id,
+            flavor_name,
+            os_version,
+            username,
+            user_pub_cert_path,
+        )
         LOG.debug(f"Template parameters for job {job_id}: {tempalte_parameters}")
         return {
             "properties": {
@@ -72,23 +83,20 @@ class AzureConnector(BaseConnector):
             }
         }
 
-    def _get_template_parameters(self, job_id: str, flavor_name: str, os_version: str, username: str, user_pub_cert_path: str,) -> dict[str, str]:
+    def _get_template_parameters(
+        self,
+        job_id: str,
+        flavor_name: str,
+        os_version: str,
+        username: str,
+        user_pub_cert_path: str,
+    ) -> dict[str, str]:
         return {
-            "resourcePrefix": {
-                "value": self._get_resource_prefix(job_id)
-            },
-            "adminUsername": {
-                "value": username
-            },
-            "adminPasswordOrKey": {
-                "value": self._get_vm_user_public_certificate(user_pub_cert_path)
-            },
-            "osVersion": {
-                "value": os_version
-            },
-            "vmSize": {
-                "value": flavor_name
-            },
+            "resourcePrefix": {"value": self._get_resource_prefix(job_id)},
+            "adminUsername": {"value": username},
+            "adminPasswordOrKey": {"value": self._get_vm_user_public_certificate(user_pub_cert_path)},
+            "osVersion": {"value": os_version},
+            "vmSize": {"value": flavor_name},
         }
 
     def _read_template(self, template_path) -> str:
@@ -96,14 +104,14 @@ class AzureConnector(BaseConnector):
             return json.load(template_file)
 
     def _get_vm_user_public_certificate(self, user_pub_cert_path: str) -> str:
-        with open(user_pub_cert_path, 'rb') as file:
+        with open(user_pub_cert_path, "rb") as file:
             pub_cert_content = file.read()
-        return pub_cert_content .decode("utf-8")
+        return pub_cert_content.decode("utf-8")
 
     def _get_pem_data(self, cert_file_path: str, key_file_path: str) -> str:
-        with open(cert_path , 'rb') as file:
+        with open(cert_path, "rb") as file:
             cert_content = file.read()
-        with open(key_file_path, 'rb') as file:
+        with open(key_file_path, "rb") as file:
             key_content = file.read()
         return key_content + cert_content
 
@@ -111,10 +119,10 @@ class AzureConnector(BaseConnector):
         return "swm-" + job_id.split("-")[0]
 
     def _get_resource_group_name(self, resource_prefix: str) -> str:
-        return f'{resource_prefix}-resource-group'
+        return f"{resource_prefix}-resource-group"
 
     def _get_deployment_name(self, resource_prefix: str) -> str:
-        return f'{resource_prefix}-deployment'
+        return f"{resource_prefix}-deployment"
 
     def list_sizes(self) -> list[AzureNodeSize]:
         if "sizes" in self._test_responses:
@@ -138,16 +146,23 @@ class AzureConnector(BaseConnector):
         # TODO
         return []
 
-    def list_images(self):
+    def list_images(self) -> list[ImageInfo]:
+        node_images: list[ImageInfo] = []
         if "images" in self._test_responses:
-            node_images = []
             for it in self._test_responses["images"]:
                 node_images.append(
-                    NodeImage(id=it["id"], name=it["name"], extra={"status": it["status"]}, driver=self._driver)
+                    ImageInfo(id=it["id"], name=it["name"], status=it["status"], created=it["created"], updated=it["updated"],)
                 )
-            return node_images
-        #TODO
-        return []
+        else:
+            azure_images = self._client.virtual_machine_images.list(
+                location=location,
+                publisher_name=publisher_name,
+                offer=offer,
+                skus=skus
+            )
+            for azure_image in azure_images:
+                images.append(azure_image.name)
+        return images
 
     def _get_cloud_init_script(self, job_id: str, runtime: str) -> str:
         runtime_params = self._get_runtime_params(runtime)
@@ -189,23 +204,29 @@ class AzureConnector(BaseConnector):
         ports: str,
         user_pub_cert_path: str,
     ) -> str:
-        #TODO use runtime, count and ports
+        # TODO use runtime, count and ports
         resource_prefix = self._get_resource_prefix(job_id)
         resource_group_name = self._get_resource_group_name(resource_prefix)
         resource_group_creation_result = self._client.resource_groups.create_or_update(
-            resource_group_name,
-            {"location": location}
+            resource_group_name, {"location": location}
         )
         LOG.info(f"Provisioned resource group with ID: {resource_group_creation_result.id}")
 
         deployment_name = self._get_deployment_name(resource_prefix)
-        deployment_properties = self._get_deployment_properties(job_id, flavor_name, os_version, username, user_pub_cert_path,)
+        deployment_properties = self._get_deployment_properties(
+            job_id,
+            flavor_name,
+            os_version,
+            username,
+            user_pub_cert_path,
+        )
         deployment_async_operation = resource_client.deployments.begin_create_or_update(
             resource_group_name,
             deployment_name,
             deployment_properties,
         )
         LOG.info(f"Deploying resource group {resource_group_name}, deployment: {deployment_name}")
+
     try:
         result = deployment_async_operation.result()
         return result
